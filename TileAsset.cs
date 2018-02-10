a60bb7af168a1d6ca7de48c9ec15bcbcf5dfe20a
Aler Denisov <aler.zampillo@gmail.com>
2018-02-11 02:41:43 +0300

    Tile data asset definition

---
+++ TileAsset.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Assets.ThreeTileRenderer
+{
+    [CreateAssetMenu(fileName = "Tile.asset", menuName = "Create Tile Asset")]
+    public class TileAsset : ScriptableObject
+    {
+        public TilePrototypeData Data;
+    }
+}
