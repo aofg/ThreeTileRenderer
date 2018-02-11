1014389ee9023a9393c0bf63ae6686e37f2f49dd
Aler Denisov <aler.zampillo@gmail.com>
2018-02-11 22:23:35 +0300

    Draft tile structure (packed to int32)

---
+++ Runtime/DataTypes/Tile.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ThreeTileRenderer.DataTypes
+{
+    public enum TileRotation
+    {
+        None,
+        CW90,
+        CW180,
+        CW270
+    }
+    
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct Tile
+    {
+        /* |Strict tile
+         * |free bits             
+         * |                 |rotation number
+         * |                 |  |collision \ solid
+         * |                 |  ||flip z
+         * |                 |  |||flip x
+         * |                 |  ||| |id number
+         * 0000 0000 0000 0000 0000 0000 0000 0000
+         * 32   28   24   20   16   12   8    4
+         */
+
+        private const int ROT_MASK = 0x3;
+        private const int ROT_MASK_INVERT = ~ROT_MASK;
+        private const int ID_MASK = 0xFFF;
+        private const int ID_MASK_INVERT = ~ID_MASK;
+
+        private const int FLIP_X_OFFSET = 12;
+        private const int FLIP_Y_OFFSET = 13;
+        private const int COLLISION_OFFSET = 14;
+        private const int ROT_OFFSET = 15;
+
+        [MarshalAs(UnmanagedType.I4)] private int raw;
+
+        public int Id
+        {
+            get { return raw & ID_MASK; }
+            set
+            {
+#if !TTR_UNSAFE
+                if (Id >= 4096 || Id < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                        "Id shoudn't be more than 4096 or less than 0");
+                }
+#endif
+                raw &= ID_MASK_INVERT;
+                raw |= value & ID_MASK;
+            }
+        }
+
+        public bool Collision
+        {
+            get { return GetBitfield(1 << COLLISION_OFFSET); }
+            set { SetBitfield(1 << COLLISION_OFFSET, value); }
+        }
+
+        public bool FlipX
+        {
+            get { return GetBitfield(1 << FLIP_X_OFFSET); }
+            set { SetBitfield(1 << FLIP_X_OFFSET, value); }
+        }
+
+        public bool FlipY
+        {
+            get { return GetBitfield(1 << FLIP_Y_OFFSET); }
+            set { SetBitfield(1 << FLIP_Y_OFFSET, value); }
+        }
+
+        public TileRotation Rotation
+        {
+            get { return (TileRotation) ((raw >> ROT_OFFSET) & ROT_MASK); }
+            set
+            {
+                // two bits (0..4) 
+                // 0: 0 CW 
+                // 1: 90 CW
+                // 2: 180 CW
+                // 3: 270 CW
+                raw &= ~(ROT_MASK << ROT_OFFSET);
+                raw |= ((int)value & ROT_MASK) << ROT_OFFSET;
+            }
+        }
+
+        private void SetBitfield(int mask, bool value)
+        {
+            if (value)
+            {
+                raw = raw | mask;
+            }
+            else
+            {
+                raw = raw & ~mask;
+            }
+        }
+
+        private bool GetBitfield(int mask)
+        {
+            return (raw & mask) != 0;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("ID: {0}, Collision: {1}, Flip: {2}, Rotate: {3}\n\n{4}",
+                Id,
+                Collision,
+                FlipX ? FlipY ? "Both" : "X" :
+                FlipY ? "Y" : "None",
+                new[] {"0 deg", "90 deg", "180 deg", "270 deg"}[(int) Rotation],
+                PrintBits(raw));
+        }
+
+        private static string PrintBits(int raw)
+        {
+            var output = new System.Text.StringBuilder();
+            var offset = 0;
+            
+            for (var index = 0; index < FLIP_X_OFFSET; index++) {
+                output.Append((raw >> offset++) & 1);
+            }
+            output.Append(' ');
+            output.Append((raw >> offset++) & 1);
+            output.Append(' ');
+            output.Append((raw >> offset++) & 1);
+            output.Append(' ');
+            output.Append((raw >> offset++) & 1);
+            output.Append(' ');
+            for (var index = 0; index < 2; index++) {
+                output.Append((raw >> offset++) & 1);
+            }
+            
+            output.Append(' ');
+            for (var index = 0; index < 5; index++) {
+                output.Append((raw >> offset++) & 1);
+            }
+            
+            output.Append(' ');
+            for (var index = 0; index < 5; index++) {
+                output.Append((raw >> offset++) & 1);
+            }
+            
+            output.Append(' ');
+            output.Append(' ');
+            while (offset < 32) {
+                output.Append((raw >> offset++) & 1);
+            }
+            
+            var charArray = output.ToString().ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
--- Runtime/DataTypes/TilePrototypeData.cs
+++ Runtime/DataTypes/TilePrototypeData.cs
@@ -1,7 +1,46 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Runtime.InteropServices;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
 
 namespace ThreeTileRenderer.DataTypes
 {
+    [StructLayout(LayoutKind.Explicit)]
+    public struct TilePrototypeStruct
+    {
+        [FieldOffset(    0)] public unsafe Vector3* Vertices;
+        [FieldOffset( 996)] public int VerticesCount;
+        [FieldOffset(1000)] public unsafe int* Triangles;
+        [FieldOffset(1996)] public int TrianglesCount;
+        [FieldOffset(2000)] public unsafe Vector3* Normals;
+        [FieldOffset(2996)] public int NormalsCount;
+        [FieldOffset(3000)] public unsafe Vector2* Uvs;
+        [FieldOffset(3996)] public int UvsCount;
+        [FieldOffset(4000)] public unsafe Vector4* Tangents;
+        [FieldOffset(4996)] public int TangentsCount;
+        [FieldOffset(5000)] public unsafe Color* Colors;
+        [FieldOffset(5996)] public int ColorsCount;
+
+        public unsafe TilePrototypeStruct(Vector3[] vertices, int[] triangles, Vector3[] normals, Vector2[] uvs, Vector4[] tangents, Color[] colors)
+        {
+            Vertices  = (Vector3*) UnsafeUtility.Malloc(UnsafeUtility.SizeOf<Vector3>() * (long) vertices.Length,  UnsafeUtility.AlignOf<Vector3>(), Allocator.Persistent);
+            Triangles = (int*)     UnsafeUtility.Malloc(UnsafeUtility.SizeOf<int>()     * (long) triangles.Length, UnsafeUtility.AlignOf<int>(),     Allocator.Persistent);
+            Normals   = (Vector3*) UnsafeUtility.Malloc(UnsafeUtility.SizeOf<Vector3>() * (long) normals.Length,   UnsafeUtility.AlignOf<Vector3>(), Allocator.Persistent);
+            Uvs       = (Vector2*) UnsafeUtility.Malloc(UnsafeUtility.SizeOf<Vector2>() * (long) uvs.Length,       UnsafeUtility.AlignOf<Vector2>(), Allocator.Persistent);
+            Tangents  = (Vector4*) UnsafeUtility.Malloc(UnsafeUtility.SizeOf<Vector4>() * (long) tangents.Length,  UnsafeUtility.AlignOf<Vector4>(), Allocator.Persistent);
+            Colors    = (Color*)   UnsafeUtility.Malloc(UnsafeUtility.SizeOf<Color>()   * (long) colors.Length,    UnsafeUtility.AlignOf<Color>(),   Allocator.Persistent);
+            
+            // set buffer lenghts
+            VerticesCount   = vertices.Length;
+            TrianglesCount  = triangles.Length;
+            NormalsCount    = normals.Length;
+            UvsCount        = uvs.Length;
+            TangentsCount   = tangents.Length;
+            ColorsCount     = colors.Length;
+        }
+    }
+    
     [System.Serializable]
     public class TilePrototypeData
     {
@@ -14,5 +53,26 @@ namespace ThreeTileRenderer.DataTypes
 
         public bool IsValid => Vertices.Length > 0 && Normals.Length == Vertices.Length && Uvs.Length == Vertices.Length &&
                                Tangents.Length == Vertices.Length && (Colors.Length == 0 || Colors.Length == Vertices.Length);
+
+        public static implicit operator TilePrototypeStruct(TilePrototypeData data)
+        {
+            return new TilePrototypeStruct(
+                data.Vertices,
+                data.Triangles,
+                data.Normals,
+                data.Uvs,
+                data.Tangents,
+                data.Colors
+            );
+            {
+//                Vertices = new NativeArray<Vector3>(data.Vertices, Allocator.Persistent),
+//                Triangles = new NativeArray<int>(data.Triangles, Allocator.Persistent),
+//                Normals = new NativeArray<Vector3>(data.Normals, Allocator.Persistent),
+//                Uvs = new NativeArray<Vector2>(data.Uvs, Allocator.Persistent),
+//                Tangents = new NativeArray<Vector4>(data.Tangents, Allocator.Persistent),
+//                Colors = new NativeArray<Color>(data.Colors, Allocator.Persistent)
+            };
+            
+        }
     }
 }
